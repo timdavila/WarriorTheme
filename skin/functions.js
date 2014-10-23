97a96e84985bd97579fddd86fe9efe9f807e1c68
timdavila <tim@timdavila.com>
2014-10-23 17:45:01 -0500

    add tablet, mobile nav

--- skin/functions.js
+++ skin/functions.js
@@ -1,11 +1,39 @@
 
-// handle show/hide menu for mobile view
+// handle show/hide menus for mobile view
 
-function ToggleMenu(menu) {
-	var e = document.getElementById(menu);
+function ToggleMenu(id) {
+	var e = document.getElementById(id);
 	if(e.style.display == 'block') {
 		e.style.display = 'none';
 	} else {
+		document.getElementById('primary').style.display = 'none';
+		document.getElementById('second').style.display = 'none';
+		document.getElementById('third').style.display = 'none';
 		e.style.display = 'block';
+		
 	}
 }
+
+
+function SwapIcon(id){ 
+	var e = document.getElementById(id);
+	if(e.className == 'rf fa fa-chevron-right') {
+		document.getElementById('icn-1').className = 'rf fa fa-chevron-right';
+		document.getElementById('icn-2').className = 'rf fa fa-chevron-right';
+		e.className = 'rf fa fa-chevron-down';
+	} else {
+		e.className = 'rf fa fa-chevron-right';
+	}
+} 
+
+function ToggleHighlight(id){ 
+	var e = document.getElementById(id);
+	if(e.className == 'btn-reg') {
+		document.getElementById('btn-1').className = 'btn-reg';
+		document.getElementById('btn-2').className = 'btn-reg';
+		document.getElementById('btn-3').className = 'btn-reg';
+		e.className = 'btn-selected';
+	} else {
+		e.className = 'btn-reg';
+	}
+} 
