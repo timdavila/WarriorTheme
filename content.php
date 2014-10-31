4a8e8c391ef4a7693d6bedbd42f4233b75cc2cf5
timdavila <tim@timdavila.com>
2014-10-30 21:22:42 -0500

    index page post template

--- content.php
+++ content.php
@@ -11,79 +11,19 @@
 ?>
 <!-- content.php -->
 
-
-		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-			<header>
-				<?php
-				if ( is_single() ) :
-					the_title( '<h1 class="entry-title">', '</h1>' );
-				else :
-					the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
-				endif; ?>
-				<p>Posted on October 19, 2014 by Tim Davila</p>
-				<p>
-				<?php
-					if ( 'post' == get_post_type() )
-					warriortheme_posted_on();
-					?>
-			</header>
-
-			<img src="http://lorempixel.com/1000/400" />
-			<?php warriortheme_post_thumbnail(); ?>
-
-			<p><strong>Lorem ipsum dolor sit amet</strong>, consectetur adipiscing elit. Quisque molestie pharetra odio quis ultrices.
-			Donec dignissim aliquam orci eget congue. Donec varius tempor augue non malesuada. Suspendisse sollicitudin risus non metus varius,
-			eget commodo lectus imperdiet. Mauris accumsan risus nibh, ac varius diam iaculis quis. Sed at arcu massa. Suspendisse potenti.
-			Fusce at eros nisl. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Donec vitae molestie eros,
-			a tristique nisl. <a href="#">Read more...</a></p>
-			<footer>
-				<div class="post-cat lf">
-				<p>Categories: <a href="#">Category 1</a>, <a href="#">Category 2</a>, <a href="#">Category 3</a></p>
-				</div>
-				<div class="post-social">
-				<p>Share this post:</p>
-					<div class="btn-share"><a href="#"><i class="fa fa-facebook"></i></a></div>
-					<div class="btn-share"><a href="#"><i class="fa fa-twitter"></i></a></div>
-					<div class="btn-share"><a href="#"><i class="fa fa-google-plus"></i></a></div>
-					<div class="btn-share"><a href="#"><i class="fa fa-reddit"></i></a></div>
-				</div>
-				<div class="cf"></div>
-			</footer>
-		</article>
-
-
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php warriortheme_post_thumbnail(); ?>
 
 	<header class="entry-header">
-		<?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) && warriortheme_categorized_blog() ) : ?>
-		<div class="entry-meta">
-			<span class="cat-links"><?php echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'warriortheme' ) ); ?></span>
-		</div>
 		<?php
 			endif;
 
 			if ( is_single() ) :
-				the_title( '<h1 class="entry-title">', '</h1>' );
+				the_title( '<h1>', '</h1>' );
 			else :
-				the_title( '<h1 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
+				the_title( '<h1><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h1>' );
 			endif;
 		?>
-
-		<div class="entry-meta">
-			<?php
-				if ( 'post' == get_post_type() )
-					warriortheme_posted_on();
-
-				if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) :
-			?>
-			<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'warriortheme' ), __( '1 Comment', 'warriortheme' ), __( '% Comments', 'warriortheme' ) ); ?></span>
-			<?php
-				endif;
-
-				edit_post_link( __( 'Edit', 'warriortheme' ), '<span class="edit-link">', '</span>' );
-			?>
-		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
 	<?php if ( is_search() ) : ?>
@@ -103,6 +43,39 @@
 		?>
 	</div><!-- .entry-content -->
 	<?php endif; ?>
+	<footer class="entry-meta">
 
-	<?php the_tags( '<footer class="entry-meta"><span class="tag-links">', '', '</span></footer>' ); ?>
+
+			<div class="entry-meta">
+				<?php
+					if ( 'post' == get_post_type() )
+						warriortheme_posted_on();
+
+					if ( ! post_password_required() && ( comments_open() || get_comments_number() ) ) :
+				?>
+				<span class="comments-link"><?php comments_popup_link( __( 'Leave a comment', 'warriortheme' ), __( '1 Comment', 'warriortheme' ), __( '% Comments', 'warriortheme' ) ); ?></span>
+				<?php
+					endif;
+
+					edit_post_link( __( 'Edit', 'warriortheme' ), '<span class="edit-link">', '</span>' );
+				?>
+		</div><!-- .entry-meta -->
+
+	<?php the_tags( '<div class="post-cat post-tag lf"><span class="tag-links">', '', '</span></div>' ); ?>
+
+			<?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) && warriortheme_categorized_blog() ) : ?>
+			<div class="post-cat lf">
+				<span class="cat-links"><?php echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'warriortheme' ) ); ?></span>
+			</div>
+
+
+			<div class="post-social">
+				<p>Share this post:</p>
+				<div class="btn-share"><a href="#"><i class="fa fa-facebook"></i></a></div>
+				<div class="btn-share"><a href="#"><i class="fa fa-twitter"></i></a></div>
+				<div class="btn-share"><a href="#"><i class="fa fa-google-plus"></i></a></div>
+				<div class="btn-share"><a href="#"><i class="fa fa-reddit"></i></a></div>
+			</div>
+			<div class="cf"></div>
+	</footer>
 </article><!-- #post-## -->
