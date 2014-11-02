54fa5ea579059cc1d09ec7d7be057ac5a913dcf3
timdavila <tim@timdavila.com>
2014-11-02 15:50:49 -0600

    separate game nav for simpler management

---
+++ gamenav.php
@@ -0,0 +1,431 @@
+<?php
+/**
+ * The game navigation for our theme
+ * Displayed only on screens 1280px and up
+ *
+ * @package WordPress
+ * @subpackage Warrior_Theme
+ * @since Warrior Theme 0.1
+ */
+?>
+		<nav class="secondary desktop cf">
+			<ul>
+				<li><a href="#"></a> <!-- 1 -->
+					<div class="flyout">
+						<div class="fly-col">
+							<h2>Flyout Title</h2>
+							<img src="http://placehold.it/230x150">
+							<p>Optional Subtitle</p>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column Header</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+								<li><a href="#">Item 4</a></li>
+								<li><a href="#">Item 5</a></li>
+								<li><a href="#">Item 6</a></li>
+								<li><a href="#">Item 7</a></li>
+								<li><a href="#">Item 8</a></li>
+								<li><a href="#">Item 9</a></li>
+								<li><a href="#">Item 10</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column Header</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+								<li><a href="#">Item 4</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column Header</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+								<li><a href="#">Item 4</a></li>
+								<li><a href="#">Item 5</a></li>
+								<li><a href="#">Item 6</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column Header</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+								<li><a href="#">Item 4</a></li>
+								<li><a href="#">Item 5</a></li>
+								<li><a href="#">Item 6</a></li>
+								<li><a href="#">Item 7</a></li>
+								<li><a href="#">Item 8</a></li>
+							</ul>
+						</div>
+					</div>
+				</li>
+				<li><a href="#"></a> <!-- 2 -->
+					<div class="flyout">
+						<div class="fly-col">
+							<h2>Flyout Title 2</h2>
+							<img src="http://lorempixel.com/230/150">
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column Header</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+								<li><a href="#">Item 4</a></li>
+								<li><a href="#">Item 5</a></li>
+								<li><a href="#">Item 6</a></li>
+								<li><a href="#">Item 7</a></li>
+								<li><a href="#">Item 8</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column Header</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+								<li><a href="#">Item 4</a></li>
+								<li><a href="#">Item 5</a></li>
+								<li><a href="#">Item 6</a></li>
+								<li><a href="#">Item 7</a></li>
+								<li><a href="#">Item 8</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column Header</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+							</ul>
+						</div>
+					</div>
+				</li>
+				<li><a href="#"></a> <!-- 3 -->
+					<div class="flyout">
+						<div class="fly-col">
+							<h2>Flyout Title 3</h2>
+							<img src="http://lorempixel.com/230/150">
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 1</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 2</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 3</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 4</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 5</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+					</div>
+				</li>
+				<li><a href="#"></a> <!-- 4 -->
+					<div class="flyout">
+						<div class="fly-col">
+							<h2>Flyout Title 4</h2>
+							<img src="http://lorempixel.com/230/150">
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 1</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 2</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 3</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 4</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 5</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 6</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 7</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 8</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+					</div>
+				</li>
+				<li><a href="#"></a> <!-- 5 -->
+					<div class="flyout">
+						<div class="fly-col">
+							<h2>Flyout Title 5</h2>
+							<img src="http://lorempixel.com/230/150">
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 1</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 2</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 3</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 4</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 5</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 6</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 7</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 8</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+					</div>
+				</li>
+				<li><a href="#"></a> <!-- 6 -->
+					<div class="flyout">
+						<div class="fly-col">
+							<h2>Flyout Title 6</h2>
+							<img src="http://lorempixel.com/230/150">
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 1</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 2</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 3</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 4</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 5</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 6</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 7</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 8</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+					</div>
+				</li>
+				<li><a href="#"></a> <!-- 7 -->
+					<div class="flyout">
+						<div class="fly-col">
+							<h2>Flyout Title 7</h2>
+							<img src="http://lorempixel.com/230/150">
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 1</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 2</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 3</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 4</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+						<div class="fly-col">
+							<ul>
+								<li>Column 5</li>
+								<li><a href="#">Item 1</a></li>
+								<li><a href="#">Item 2</a></li>
+								<li><a href="#">Item 3</a></li>
+							</ul>
+						</div>
+					</div>
+				</li>
+				<li><a href="#"></a></li>
+				<li><a href="#"></a></li>
+				<li><a href="#"></a></li>
+				<li><a href="#"></a></li>
+				<li><a href="#"></a></li>
+				<li><a href="#"></a></li>
+				<li><a href="#"></a></li>
+				<li><a href="#"></a></li>
+				<li><a href="#"></a></li>
+				<li><a href="#"></a></li>
+				<li><a href="#"></a></li>
+				<li><a href="#"></a></li> <!-- 19 -->
+			</ul>
+		</nav>
