1a2a71f75d80dea3ebcd7225a7422fb5fe22cba1
timdavila <tim@timdavila.com>
2014-11-07 08:30:14 -0600

    yep this is js not php

--- js/functions.js
+++ js/functions.js
@@ -3,13 +3,13 @@
  *
  */
  
-$menus = array("primary", "second", "third");
+var menus = ["primary", "second", "third"];
  
  // handle show/hide menus for mobile view
  
  function ToggleMenu(id) {
  	
- 	var e = document.getElementById($menus[id-1]);
+ 	var e = document.getElementById(menus[id-1]);
  	if(e.style.display == 'block') {
  		e.style.display = 'none';
  		document.getElementById('btn-1').className = 'btn-reg';
@@ -18,17 +18,17 @@ $menus = array("primary", "second", "third");
  		document.getElementById('icn-1').className = 'rf fa fa-chevron-right';
  		document.getElementById('icn-2').className = 'rf fa fa-chevron-right';
  	} else {
- 		document.getElementById($menus[0]).style.display = 'none';
- 		document.getElementById($menus[1]).style.display = 'none';
- 		document.getElementById($menus[2]).style.display = 'none';
+ 		document.getElementById(menus[0]).style.display = 'none';
+ 		document.getElementById(menus[1]).style.display = 'none';
+ 		document.getElementById(menus[2]).style.display = 'none';
  		document.getElementById('btn-1').className = 'btn-reg';
  		document.getElementById('btn-2').className = 'btn-reg';
  		document.getElementById('btn-3').className = 'btn-reg';
  		document.getElementById('icn-1').className = 'rf fa fa-chevron-right';
  		document.getElementById('icn-2').className = 'rf fa fa-chevron-right';
  		e.style.display = 'block';
- 		document.getElementById('btn-'.id).className = 'btn-selected';
- 		document.getElementById('icn-'.id).className = 'rf fa fa-chevron-down';
+ 		document.getElementById('btn-'+id).className = 'btn-selected';
+ 		document.getElementById('icn-'+id).className = 'rf fa fa-chevron-down';
  		
  	}
  }
