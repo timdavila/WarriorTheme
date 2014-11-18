6f5dc62fb95cc9d14f1b044581c9c590eaa04e17
timdavila <tim@timdavila.com>
2014-11-18 12:46:31 -0600

    404 page format

--- 404.php
+++ 404.php
@@ -9,24 +9,24 @@
 
 get_header(); ?>
 
+<!-- template 404.php -->
 
-	<div id="primary" class="content-area">
-		<div id="content" class="site-content" role="main">
+<div class="sitewrap">
+	<main id="content" role="main">
 
-			<header class="page-header">
+		<article>
+			<header class="entry-header">
 				<h1 class="page-title"><?php _e( 'Not Found', 'warriortheme' ); ?></h1>
 			</header>
 
-			<div class="page-content">
+			<div class="entry-content">
 				<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'warriortheme' ); ?></p>
 
 				<?php get_search_form(); ?>
-			</div><!-- .page-content -->
-
-		</div><!-- #content -->
-	</div><!-- #primary -->
+			</div>
+		</article>
+	</main>
 
 <?php
-get_sidebar( 'content' );
 get_sidebar();
 get_footer();
