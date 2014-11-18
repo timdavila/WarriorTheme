5b2685997c12eacf56d4f0bfc4b390242610bbc3
timdavila <tim@timdavila.com>
2014-11-18 07:58:23 -0600

    show previous and next link only in site pagination

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -24,42 +24,11 @@ function warriortheme_paging_nav() {
 		return;
 	}
 
-
-	next_posts_link( '&larr; Older Entries', $wp_query->max_num_pages );
-	previous_posts_link( 'Newer Entries &rarr;' );
-
-	$paged        = get_query_var( 'paged' ) ? intval( get_query_var( 'paged' ) ) : 1;
-	$pagenum_link = html_entity_decode( get_pagenum_link() );
-	$query_args   = array();
-	$url_parts    = explode( '?', $pagenum_link );
-
-	if ( isset( $url_parts[1] ) ) {
-		wp_parse_str( $url_parts[1], $query_args );
-	}
-
-	$pagenum_link = remove_query_arg( array_keys( $query_args ), $pagenum_link );
-	$pagenum_link = trailingslashit( $pagenum_link ) . '%_%';
-
-	$format  = $wp_rewrite->using_index_permalinks() && ! strpos( $pagenum_link, 'index.php' ) ? 'index.php/' : '';
-	$format .= $wp_rewrite->using_permalinks() ? user_trailingslashit( $wp_rewrite->pagination_base . '/%#%', 'paged' ) : '?paged=%#%';
-
-	// Set up paginated links.
-	$links = paginate_links( array(
-		'base'     => $pagenum_link,
-		'format'   => $format,
-		'total'    => $wp_query->max_num_pages,
-		'current'  => $paged,
-		'mid_size' => 0,
-		'add_args' => array_map( 'urlencode', $query_args ),
-		'prev_text' => __( '&larr; Older Entries', 'warriortheme' ),
-		'next_text' => __( 'Newer Entries &rarr;', 'warriortheme' ),
-	) );
-
-	if ( $links ) :
-
-	?>
-	<nav class="pagination" role="navigation">
-		<?php echo $links; ?>
+	<nav class="pagination" role="navigation"><span class="prev-link lf">
+	<?php next_posts_link( '&larr; Older Entries', $wp_query->max_num_pages ); ?>
+	</span><span class="next-link rf">
+	<?php previous_posts_link( 'Newer Entries &rarr;' ); ?>
+	</span>
 	</nav>
 	<?php
 	endif;
