841fdcf80de587b1b82d77626ceb7d39eeb238b5
timdavila <tim@timdavila.com>
2014-11-18 12:25:34 -0600

    fix for search widget in sidebar

--- searchform.php
+++ searchform.php
@@ -14,5 +14,5 @@
 	<input type="search" class="in-search" placeholder="<?php echo esc_attr_x( 'Search this site...', 'placeholder' ) ?>" value="<?php echo get_search_query() ?>" name="s" title="<?php echo esc_attr_x( 'Search this site', 'label' ) ?>" required />
 	<button type="submit" class="btn-search"><i class="fa fa-search"></i></button>
 </form>
-
+<div class="cf"></div>
 <!-- eof searchform.php -->
